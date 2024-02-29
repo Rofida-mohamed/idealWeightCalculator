a1095c08c217d688782e51481bab78ffaf87cabf
Rofida_2000 <mohamedrofida7@gmail.com>
2024-02-29 12:54:15 +0200

    Add project files.

---
+++ IdealWeightTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IdealWeightTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IdealWeightTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("195283a0-ce44-4900-91ff-5c252243335e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ idealWeightCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace idealWeightCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IdealWeight weight_01 = new IdealWeight
+            {
+                Height = 176,
+                Gender = 'w'
+            };
+
+            int weight = weight_01.GetIdealWeight();
+            Console.WriteLine($"Weight = {weight}");
+            int expected = 64;
+            //for Manual testing 
+            if (weight == expected)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("failed");
+            }
+        }
+    }
+}
